b201fa05bc22b2de2a0b33afcbadeaee27064a3c
merlijnkersten <kerstenmerlijn@gmail.com>
2019-06-02 17:03:56 +0100

    Update AutomatedRenamingOfPhotos.py

--- AutomatedRenamingOfPhotos.py
+++ AutomatedRenamingOfPhotos.py
@@ -32,31 +32,36 @@
 
 # https://stackoverflow.com/questions/19501711/how-can-i-convert-os-path-getctime
 # For correct date+time
-'''
+
 import os
 from datetime import datetime
 
-os.chdir(r'C:\\Users\\Merlijn Kersten\\Pictures\\') #Sets directory
+os.chdir(r"C:\\Users\\Merlijn Kersten\\Pictures\\Photos\\soloespresso\\AutomaticResizingAttempt") #Sets directory
 
-old_path = "me.jpg" #location of original file
-a = os.path.getctime(old_path) #creation date (computer)
-b = datetime.fromtimestamp(a).strftime('%Y-%m-%d') #creation date (human-readable)
-c = "soloespresso" + b +".jpg" #correct filename
-print(c)
-new_path = os.path.join("Renamed", c)
-print(new_path)
-os.rename(old_path, new_path)
-'''
-'''
-print(True == 1)
+i = 2 #REQUIRES FILES TO BE NAMED CONSECUTIVELY 
+old_path = str(i) + ".jpg"
 
-if os.path.isfile(new_path) != True:
-    print("A file with that name did not yet exist.")
-    os.rename(old_path, new_path)
-    print("It does now.")
-else:
-    print("A file with that name already exists.")
-    new_c = "attention_needed_" + c
-    new_path = os.path.join("Renamed" + new_c)
-    print("A file with 'attention needed' + file name was created instead.")
-'''
+while os.path.isfile(old_path) == True:
+    a = os.path.getctime(old_path) #creation date (computer)
+    b = datetime.fromtimestamp(a).strftime('%Y-%m-%d') #creation date (human-readable)
+    c = "soloespresso" + b +".jpg" #correct filename
+    new_path = os.path.join("Renamed", c)
+    if os.path.isfile(new_path) != True:
+        os.rename(old_path, new_path)
+        d = "Image #" + str(i) + " is now: " + c  
+        print(d)
+    else:
+        new_c = "attention_needed_" + c
+        new_path = os.path.join("Renamed", new_c)
+        os.rename(old_path, new_path)
+        d = "Attention need for file" + c
+        print(d)
+    i += 1
+    old_path = str(i) + ".jpg"
+
+print("Stoped at image: " + str(i) + ".jpg")
+
+
+import os
+rootdir = 'C:/Users/sid/Desktop/test'
+
