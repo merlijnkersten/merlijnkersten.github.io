4780d53ebc7414b05d1be6c809fea742f9e1aff3
merlijnkersten <kerstenmerlijn@gmail.com>
2019-06-02 16:31:59 +0100

    Update AutomatedRenamingOfPhotos.py

--- AutomatedRenamingOfPhotos.py
+++ AutomatedRenamingOfPhotos.py
@@ -30,9 +30,32 @@
 # p = Path(some_path)
 # p.rename(Path(p.parent, "{}_{}".format(p.stem, 1) + p.suffix))
 
-print("Try 1")
+# https://stackoverflow.com/questions/19501711/how-can-i-convert-os-path-getctime
+# For correct date+time
 
 import os
-path = os.path.join("C:", "Users", "Merlijn Kersten", "Pictures", "me.jpg")
-a = os.path.getctime(r"C:\Users\Merlijn Kersten\Pictures\me.jpg")
-print(a)
+from datetime import datetime
+
+os.chdir(r'C:\\Users\\Merlijn Kersten\\Pictures\\') #Sets directory
+
+old_path = "me.jpg" #location of original file
+a = os.path.getctime(old_path) #creation date (computer)
+b = datetime.fromtimestamp(a).strftime('%Y-%m-%d') #creation date (human-readable)
+c = "soloespresso" + b +".jpg" #correct filename
+print(c)
+new_path = os.path.join("Renamed", c)
+print(new_path)
+os.rename(old_path, new_path)
+'''
+print(True == 1)
+
+if os.path.isfile(new_path) != True:
+    print("A file with that name did not yet exist.")
+    os.rename(old_path, new_path)
+    print("It does now.")
+else:
+    print("A file with that name already exists.")
+    new_c = "attention_needed_" + c
+    new_path = os.path.join("Renamed" + new_c)
+    print("A file with 'attention needed' + file name was created instead.")
+'''
